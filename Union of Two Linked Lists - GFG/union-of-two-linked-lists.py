ddae8270eecc8bdd5a7b04c2ce8cc0e8f7de3ebd
AVINASH DUBEY <125783004+Avinash4006@users.noreply.github.com>
2023-06-27 22:45:09 +0530

    Added solution - LeetHub

---
+++ Union of Two Linked Lists - GFG/union-of-two-linked-lists.py
@@ -0,0 +1,72 @@
+class Solution:
+    def union(self, head1, head2):
+        distinct_set = set()
+
+        current = head1
+        while current:
+            distinct_set.add(current.data)
+            current = current.next
+        current = head2
+        while current:
+            distinct_set.add(current.data)
+            current = current.next
+
+        sorted_list = sorted(distinct_set)
+
+        dummy = Node(0)
+        tail = dummy
+        for num in sorted_list:
+            tail.next = Node(num)
+            tail = tail.next
+        return dummy.next
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class linkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    
+    def insert(self,data):
+        if self.head is None:
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(data)
+            self.tail = self.tail.next
+
+def printList(head):
+    while head:
+        print(head.data,end=' ')
+        head=head.next
+        
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        
+        n1 = int(input())
+        arr1 = [int(x) for x in input().split()]
+        ll1 = linkedList()
+        for i in arr1:
+            ll1.insert(i)
+        
+        n2 = int(input())
+        arr2 = [int(x) for x in input().split()]
+        ll2 = linkedList()
+        for i in arr2:
+            ll2.insert(i)
+        
+        ob = Solution()
+        printList(ob.union(ll1.head,ll2.head))
+        print()
+
+# } Driver Code Ends

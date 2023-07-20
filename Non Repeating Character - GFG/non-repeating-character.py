4ba2eb5971b8c8b47b1b69fcf53520eed754fb90
AVINASH DUBEY <125783004+Avinash4006@users.noreply.github.com>
2023-07-20 21:05:32 +0530

    Added solution - LeetHub

---
+++ Non Repeating Character - GFG/non-repeating-character.py
@@ -0,0 +1,53 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the first non-repeating character in a string.
+    def nonrepeatingCharacter(self,s):
+        #code here
+        char_freq = {}
+
+        # Count the frequency of each character
+        for char in s:
+            char_freq[char] = char_freq.get(char, 0) + 1
+
+        # Find the first non-repeating character
+        for char in s:
+            if char_freq[char] == 1:
+                return char
+
+        # If no non-repeating character is found, return '$'
+        return '$' 
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        s=str(input())
+        obj = Solution()
+        ans=obj.nonrepeatingCharacter(s)
+        if(ans!='$'):
+            print(ans)
+        else:
+            print(-1)
+            
+# } Driver Code Ends
